88c4ea94175563edfd3b1c6f0ca627c6780bfaa4
anshulbisht06 <anshul.bisht06@gmail.com>
2016-04-12 15:48:23 +0530

    cached removed from server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect'), serveStatic = require('serve-static');
-
-var app = connect();
-
-app.use(serveStatic("dist"));
-app.listen(9000, function(){
-        console.log('LIVE >>>> listening on 9000 port');
-    console.log(__dirname);
-});
